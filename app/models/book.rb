3e460ad2683c39cc410fb209bb3bc2f9bc56ef54
EC2 Default User <ec2-user@ip-172-31-82-51.ec2.internal>
2022-03-07 08:14:38 +0000

    update

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,8 +8,24 @@ class Book < ApplicationRecord
   validates :body, presence: true,
                    length: { maximum: 200}
 
+  #いいね機能
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
+  end
+
+  #検索方法分岐機能
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("name LIKE?", "%#{word}%")
+    else
+      @book = Book.all
+    end
   end
 
 end
